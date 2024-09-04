3942174f3720c6ff914701a3463e5d8c3ddc1d6d
AranBeitia <arantza.beitia@gmail.com>
2024-09-04 19:02:29 +0200

    loading

--- src/components/task-list/task-list-form/TaskListForm.jsx
+++ src/components/task-list/task-list-form/TaskListForm.jsx
@@ -1,33 +1,41 @@
-import {FaPlus} from 'react-icons/fa6'
-import './TaskListForm.scss'
-
 import {useState, useContext} from 'react'
 import {GlobalContext} from '../../../context/GlobalState'
+import {FaPlus} from 'react-icons/fa6'
+import Loader from '../../loader/Loader'
+import './TaskListForm.scss'
 
 const TaskListForm = () => {
   const {createTask} = useContext(GlobalContext)
   const [task, setTask] = useState({title: ''})
+  const [loading, setLoading] = useState(false)
 
-  const addTask = (event) => {
+  const addTask = async (event) => {
     event.preventDefault()
-    createTask(task)
-    console.log('addTask', task)
+    setLoading(true)
+    await createTask(task)
     setTask({title: ''})
+    setLoading(false)
   }
 
   return (
-    <form className='task-form' onSubmit={addTask}>
-      <input
-        type='text'
-        placeholder='Add a new task'
-        className='task-form__input'
-        value={task.title}
-        onChange={(e) => setTask({title: e.target.value})}
-      />
-      <button className='primary-button'>
-        <FaPlus />
-      </button>
-    </form>
+    <>
+      {!loading ? (
+        <form className='task-form' onSubmit={addTask}>
+          <input
+            type='text'
+            placeholder='Add a new task'
+            className='task-form__input'
+            value={task.title}
+            onChange={(e) => setTask({title: e.target.value})}
+          />
+          <button className='primary-button'>
+            <FaPlus />
+          </button>
+        </form>
+      ) : (
+        <Loader />
+      )}
+    </>
   )
 }
 
--- src/components/task-list/task-list-item/TaskListItem.jsx
+++ src/components/task-list/task-list-item/TaskListItem.jsx
@@ -1,25 +1,42 @@
+import {useContext, useState} from 'react'
+import {GlobalContext} from '../../../context/GlobalState'
 import {MdCheck} from 'react-icons/md'
 import {MdDeleteOutline} from 'react-icons/md'
 import './TaskListItem.scss'
-
-import {useContext} from 'react'
-import {GlobalContext} from '../../../context/GlobalState'
+import Loader from '../../loader/Loader'
 
 const TaskListItem = ({id, title, completed}) => {
   const {deleteTask, completeTask} = useContext(GlobalContext)
+  const [loading, setLoading] = useState(false)
+
+  const handleDelete = async () => {
+    setLoading(true)
+    await deleteTask(id)
+    setLoading(false)
+  }
+
+  const handleComplete = async () => {
+    setLoading(true)
+    await completeTask(id)
+    setLoading(false)
+  }
 
   return (
     <div className='list-item' key={id}>
       <p className={completed ? 'list-item--is-done' : ''}>{title}</p>
       <div>
-        {!completed && (
-          <button className='action-button' onClick={() => completeTask(id)}>
+        {!completed && !loading && (
+          <button className='action-button' onClick={handleComplete}>
             <MdCheck />
           </button>
         )}
-        <button className='action-button' onClick={() => deleteTask(id)}>
-          <MdDeleteOutline />
-        </button>
+        {!loading ? (
+          <button className='action-button' onClick={handleDelete}>
+            <MdDeleteOutline />
+          </button>
+        ) : (
+          <Loader />
+        )}
       </div>
     </div>
   )
